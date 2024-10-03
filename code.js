8f827b9257920784180bce392bb7a8eeba88c3d6
Mamadou Saliou DIALLO <kewere097@gmail.com>
2024-10-03 13:39:40 +0000

    first commit

---
+++ code.js
@@ -0,0 +1,34 @@
+// Déclaration des variables
+let a = 5; // Tu peux changer ces valeurs
+let b = 10;
+let c = 2;
+
+// Calcul de la somme
+let somme = a + b + c;
+
+// Affichage du résultat dans la console
+console.log("La somme des trois variables est :", somme);
+
+
+// Calcul du produit
+let produit = a * b * c;
+
+// Affichage du résultat par alerte
+alert("Le produit des trois variables est : " + produit);
+
+
+// Programme pour afficher la table de multiplication de 1 à 9
+for (let i = 1; i <= 9; i++) {
+    for (let j = 1; j <= 10; j++) {
+        console.log(`${i} x ${j} = ${i * j}`);
+    }
+}
+
+// Calcul de la somme de a et b
+let sommeAB = a + b;
+
+// Division de la somme par c
+let resultatDivision = sommeAB / c;
+
+// Affichage du résultat
+console.log("Le résultat de la somme de a et b divisé par c est :", resultatDivision);
